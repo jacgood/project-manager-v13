b49cc79f73c7eec2f4c2d1594017469244f12fe3
Jacob Good <jgood@good-it-solutions.com>
2020-07-08 15:24:56 -0400

    refactored so the api can update the project as well as the objectives and tasks.

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken');
 const { body, validationResult } = require('express-validator');
 
 const Project = require('../../models/Project');
-const e = require('express');
 
 router.use((req, res, next) => {
   const token = req.headers['access-token'];
@@ -69,26 +68,27 @@ router.put('/:id', (req, res) => {
   const { objectiveId, taskId } = req.query;
   const { objective, task } = req.body;
 
-  Project.findById(id, (err, project) => {
+  Project.findByIdAndUpdate(id, req.body, (err, project) => {
     if (err) {
       return res.status(500).json(err);
     }
-
     const obj = project.objectives.id(objectiveId);
 
     if (!objectiveId && !taskId) {
-      // console.log('creating objective ...');
-      project.objectives.push(objective);
+      if (objective) {
+        // add an objective
+        project.objectives.push(objective);
+      }
     } else if (objectiveId && !taskId) {
       if (objective) {
-        // console.log('updating objective ...');
+        // update the objective
         obj.set(objective);
       } else {
-        // console.log('creating task ...');
+        // add a task
         obj.tasks.push(task);
       }
     } else if (objectiveId && taskId) {
-      // console.log('updating task ...');
+      // update the task
       const tsk = obj.tasks.id(taskId);
       tsk.set(task);
     }
@@ -103,5 +103,90 @@ router.put('/:id', (req, res) => {
       });
   });
 });
+
+// router.put('/:id', (req, res) => {
+//   const { id } = req.params;
+//   const { objectiveId, taskId } = req.query;
+//   const { project, objective, task } = req.body;
+
+//   Project.findById(id, (err, proj) => {
+//     if (err) {
+//       return res.status(500).json(err);
+//     }
+
+//     const obj = proj.objectives.id(objectiveId);
+
+//     if (!objectiveId && !taskId) {
+//       if (!objective) {
+//         // update the project
+//         proj = project;
+//       } else {
+//         // add an objective
+//         proj.objectives.push(objective);
+//       }
+//     } else if (objectiveId && !taskId) {
+//       if (objective) {
+//         // update the objective
+//         obj.set(objective);
+//       } else {
+//         // add a task
+//         obj.tasks.push(task);
+//       }
+//     } else if (objectiveId && taskId) {
+//       // update the task
+//       const tsk = obj.tasks.id(taskId);
+//       tsk.set(task);
+//     }
+
+//     proj
+//       .save()
+//       .then(proj => {
+//         res.json(project);
+//       })
+//       .catch(err => {
+//         console.log(err); // eslint-disable-line
+//       });
+//   });
+// });
+
+// router.delete('/:id', (req, res) => {
+//   const { id } = req.params;
+//   const { objectiveId, taskId } = req.query;
+//   const { objective, task } = req.body;
+
+//   Project.findById(id, (err, project) => {
+//     if (err) {
+//       return res.status(500).json(err);
+//     }
+
+//     const obj = project.objectives.id(objectiveId);
+
+//     if (!objectiveId && !taskId) {
+//       // console.log('creating objective ...');
+//       project.objectives.push(objective);
+//     } else if (objectiveId && !taskId) {
+//       if (objective) {
+//         // console.log('updating objective ...');
+//         obj.set(objective);
+//       } else {
+//         // console.log('creating task ...');
+//         obj.tasks.push(task);
+//       }
+//     } else if (objectiveId && taskId) {
+//       // console.log('updating task ...');
+//       const tsk = obj.tasks.id(taskId);
+//       tsk.set(task);
+//     }
+
+//     project
+//       .save()
+//       .then(project => {
+//         res.json(project);
+//       })
+//       .catch(err => {
+//         console.log(err); // eslint-disable-line
+//       });
+//   });
+// });
 
 module.exports = router;
