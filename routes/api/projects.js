37e648ea05fa91cd88df2fabc78ad170f7537787
Jacob Good <jgood@good-it-solutions.com>
2020-07-08 16:15:49 -0400

    added full crud functionality to projects, objectives, and tasks.

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -104,89 +104,34 @@ router.put('/:id', (req, res) => {
   });
 });
 
-// router.put('/:id', (req, res) => {
-//   const { id } = req.params;
-//   const { objectiveId, taskId } = req.query;
-//   const { project, objective, task } = req.body;
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  const { objectiveId, taskId } = req.query;
 
-//   Project.findById(id, (err, proj) => {
-//     if (err) {
-//       return res.status(500).json(err);
-//     }
+  Project.findById(id, (err, project) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
 
-//     const obj = proj.objectives.id(objectiveId);
+    if (!objectiveId && !taskId) {
+      return res.send(project.remove());
+    }
 
-//     if (!objectiveId && !taskId) {
-//       if (!objective) {
-//         // update the project
-//         proj = project;
-//       } else {
-//         // add an objective
-//         proj.objectives.push(objective);
-//       }
-//     } else if (objectiveId && !taskId) {
-//       if (objective) {
-//         // update the objective
-//         obj.set(objective);
-//       } else {
-//         // add a task
-//         obj.tasks.push(task);
-//       }
-//     } else if (objectiveId && taskId) {
-//       // update the task
-//       const tsk = obj.tasks.id(taskId);
-//       tsk.set(task);
-//     }
+    if (objectiveId && !taskId) {
+      project.objectives.pull(objectiveId);
+    } else if (objectiveId && taskId) {
+      project.objectives.id({ _id: objectiveId }).tasks.pull(taskId);
+    }
 
-//     proj
-//       .save()
-//       .then(proj => {
-//         res.json(project);
-//       })
-//       .catch(err => {
-//         console.log(err); // eslint-disable-line
-//       });
-//   });
-// });
-
-// router.delete('/:id', (req, res) => {
-//   const { id } = req.params;
-//   const { objectiveId, taskId } = req.query;
-//   const { objective, task } = req.body;
-
-//   Project.findById(id, (err, project) => {
-//     if (err) {
-//       return res.status(500).json(err);
-//     }
-
-//     const obj = project.objectives.id(objectiveId);
-
-//     if (!objectiveId && !taskId) {
-//       // console.log('creating objective ...');
-//       project.objectives.push(objective);
-//     } else if (objectiveId && !taskId) {
-//       if (objective) {
-//         // console.log('updating objective ...');
-//         obj.set(objective);
-//       } else {
-//         // console.log('creating task ...');
-//         obj.tasks.push(task);
-//       }
-//     } else if (objectiveId && taskId) {
-//       // console.log('updating task ...');
-//       const tsk = obj.tasks.id(taskId);
-//       tsk.set(task);
-//     }
-
-//     project
-//       .save()
-//       .then(project => {
-//         res.json(project);
-//       })
-//       .catch(err => {
-//         console.log(err); // eslint-disable-line
-//       });
-//   });
-// });
+    project
+      .save()
+      .then(project => {
+        res.json(project);
+      })
+      .catch(err => {
+        console.log(err); // eslint-disable-line
+      });
+  });
+});
 
 module.exports = router;
